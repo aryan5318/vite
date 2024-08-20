ca2272e8c9f5517cefe97143f7fba653a06cd405
aryan5318 <140104378+aryan5318@users.noreply.github.com>
2024-08-20 16:32:48 +0530

    Update contact.jsx

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Contact.css'; 
+import './contact.css'; 
 import { FaLinkedin } from "react-icons/fa6";
 import { CiInstagram } from "react-icons/ci";
 import { FaGitSquare } from "react-icons/fa";
